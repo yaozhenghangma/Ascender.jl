e696bac3a302b145e4e2db5bdf663d8562b02f83
Documenter.jl <documenter@juliadocs.github.io>
2022-03-30 08:11:03 +0000

    build based on f0a3487

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Ascender","category":"page"},{"location":"#Ascender","page":"Home","title":"Ascender","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Ascender.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Ascender]","category":"page"}]
+}
